83c03c8cbd3bf7a8f6bdd42c31aae6a6a66ddfae
s3xmaxman <badside3919@gmail.com>
2024-12-17 07:08:57 +0900

    Integrate Clerk authentication: added @clerk/clerk-expo dependency, updated package.json and package-lock.json, and implemented ClerkProvider in the layout for authentication management.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,28 @@
 import { Stack } from "expo-router";
+import { LogBox } from "react-native";
+import { ClerkProvider } from "@clerk/clerk-expo";
 
-export default function RootLayout() {
-  return <Stack />;
+const CLERK_PUBLISHABLE_KEY = process.env
+  .EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
+
+if (!CLERK_PUBLISHABLE_KEY) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
 }
+
+LogBox.ignoreLogs(["Clerk: Clerk has been loaded with development keys"]);
+
+const InitialLayout = () => {
+  return <Stack />;
+};
+
+const RootLayout = () => {
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+      <InitialLayout />
+    </ClerkProvider>
+  );
+};
+
+export default RootLayout;
