13ed26e9deb61622bc9ac4b80a8603f8fde8907f
s3xmaxman <badside3919@gmail.com>
2024-12-19 02:17:32 +0900

    Update package dependencies and enhance today screen functionality: added date-fns and react-native-bouncy-checkbox to package.json and package-lock.json. Refactored today screen to utilize SQLite for data management with drizzle-orm, implementing live queries for todos and projects.

--- app/(authenticated)/(tabs)/today/index.tsx
+++ app/(authenticated)/(tabs)/today/index.tsx
@@ -1,7 +1,46 @@
-import { View, Text } from "react-native";
-import React from "react";
+import Fab from "@/components/Fab";
+import { useSQLiteContext } from "expo-sqlite";
+import {
+  StyleSheet,
+  Text,
+  RefreshControl,
+  View,
+  SectionList,
+} from "react-native";
+import { useState, useEffect } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Colors } from "@/constants/Colors";
+import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite";
+import { projects, todos } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
+import Animated, {
+  StretchInY,
+  LayoutAnimationConfig,
+} from "react-native-reanimated";
 
 const Page = () => {
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db);
+
+  useDrizzleStudio(db);
+
+  const { data } = useLiveQuery(
+    drizzleDb
+      .select()
+      .from(todos)
+      .leftJoin(projects, eq(todos.project_id, projects.id))
+      .where(eq(todos.completed, 0))
+  );
+
+  useEffect(() => {
+    const formattedData = data?.map((item) => ({
+      ...item.todos,
+      project_name: item.projects?.name,
+      project_color: item.projects?.color,
+    }));
+  }, [data]);
+
   return (
     <View>
       <Text>Page</Text>
